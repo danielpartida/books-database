ddb3e80b4d78c84a1451f8b0e201b981d27f64a9
daniel <partidapd@gmail.com>
2023-02-02 16:11:07 +0100

    Create request method to download an book

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# books-database
+# Books Database Repository
 Repository to create a database of books
+
+## Instructions
+- Download [Adobe Digital Editions](https://www.adobe.com/solutions/ebook/digital-editions/download.html) to read the audiobooks
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import requests
 
 
+# TODO: Store fields of metadata title, language, published, author, publisher, description, subject
 def get_book_meta_data(book_number: int = 2936) -> dict:
     base_url = "https://www.feedbooks.com/"
     book_url = base_url + "book/{0}".format(book_number)
@@ -11,8 +12,19 @@ def get_book_meta_data(book_number: int = 2936) -> dict:
     return data
 
 
+def download_book(book_number: int = 2936, title: str = "Romeo and Juliet"):
+    title = title.replace(" ", '-').lower()
+    base_url = "https://download.feedbooks.net/"
+    download_url = base_url + "book/{0}.epub?filename={1}".format(book_number, title)
+
+    response = requests.get(url=download_url)
+    # Raise error in case of an HTTPError
+    response.raise_for_status()
+    content = response.content
+    open('../db/{0}_{1}.epub'.format(book_number, title), 'wb').write(content)
+
+
 if __name__ == "__main__":
 
     data = get_book_meta_data()
-
-    print(data)
+    download_book()
